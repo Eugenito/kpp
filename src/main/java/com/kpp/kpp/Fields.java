51b1b4e56494c7243be7a02bb27d32ed183ee9d7
Eugenito <geniousmont@gmail.com>
2022-04-04 00:03:19 +0300

    the first lab work performed

---
+++ src/main/java/com/kpp/kpp/Fields.java
@@ -0,0 +1,34 @@
+package com.kpp.kpp;
+
+public class Fields {
+    private String field1;
+    private String field2;
+    private String field3;
+    public Fields(String value)
+    {
+        this.field1=value;
+
+    }
+    public String getField1()
+    {
+        return field1;
+    }
+    public String getField2()
+    {
+        return field2;
+    }
+    public String getField3()
+    {
+        return field3;
+    }
+    public void setField2(String value)
+    {
+        this.field2=value;
+
+    }
+    public void setField3(String value)
+    {
+        this.field3=value;
+    }
+
+}
