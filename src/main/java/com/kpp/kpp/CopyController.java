480f48297d8c1c065a5b084e8c01c740d540775a
Hanna_Akhrameika <aneanra@yandex.ru>
2022-04-04 17:57:13 +0300

    final commit

--- src/main/java/com/kpp/kpp/CopyController.java
+++ src/main/java/com/kpp/kpp/CopyController.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CopyController {
     @GetMapping("/copy")
-    public Fields copyValue(@RequestParam(value="value")String value)
-    {
-        Fields fields =new Fields(value);
+    public Fields copyValue(@RequestParam(value = "value") String value) {
+        Fields fields = new Fields();
         fields.setField2(value);
         fields.setField3(value);
         return fields;
     }
-
 }
